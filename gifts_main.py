f0dcf9b2131f1cc141c8988734620be8b67d6174
Dataloops-code <dataloopskw.code@gmail.com>
2025-01-30 06:32:12 +0200

    Update gifts_main.py

--- gifts_main.py
+++ gifts_main.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 from typing import Dict, List, Tuple
 from pathlib import Path
 from DetailsScraper import DetailsScraping
-from SavingOnDrive import SavingOnDrive
+from SavingOnDriveGifts import SavingOnDriveGifts
 
 
 class GiftsMainScraper:
@@ -138,7 +138,7 @@ class GiftsMainScraper:
                 self.logger.info("Environment variable GIFTS_GCLOUD_KEY_JSON is set.")
 
             credentials_dict = json.loads(credentials_json)
-            drive_saver = SavingOnDrive(credentials_dict)
+            drive_saver = SavingOnDriveGifts(credentials_dict)
             drive_saver.authenticate()
             self.logger.info("Testing Drive API access...")
             try:
